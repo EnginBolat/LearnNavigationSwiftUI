2a370d3691dc1f43ddbdd8557dc830372150f612
Engin Bolat <engi.bolat@gmail.com>
2025-06-30 21:57:25 +0300

    Initial Commit

---
+++ LearnNavigationSwiftUI/LearnNavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnNavigationSwiftUIApp.swift
+//  LearnNavigationSwiftUI
+//
+//  Created by Engin Bolat on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct LearnNavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
